0bde02eab6b2677e9dfb5713909b2314d380c053
Xavier Logerais <xavier.logerais-ext@malakoffhumanis.com>
2023-11-02 18:29:02 +0100

    feat: Split config for colorscheme

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,5 +1,4 @@
 return {
-  colorscheme = "astrodark",
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
